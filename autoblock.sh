8c080c921faf9594a95bc64fb8d49d13e00157f6
Hiep Dang Van <hiepdangvan.iv@gmail.com>
2016-05-25 13:55:29 +0700

    Update

--- autoblock.sh
+++ autoblock.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+
 ### nano /etc/cron.daily/autoblock.sh
 ### chmod +x /etc/cron.daily/autoblock.sh
+### sh /etc/cron.daily/autoblock.sh
+### nginx -t && service nginx restart
 #### piwik spam blocklist
+
 mkdir -p /home/gagomap/tools/referrer_spam
 \curl -sSL https://raw.githubusercontent.com/gagomap/tools/master/referrer_spam/nginx-refspam-config-gen.py > /home/gagomap/tools/referrer_spam/nginx-refspam-config-gen.py
 
@@ -15,17 +19,18 @@ echo "Install referer blacklist Stevie-Ray (bad_referer) "
 \curl -sSL https://raw.githubusercontent.com/Stevie-Ray/apache-nginx-referral-spam-blacklist/master/referral-spam.conf > /etc/nginx/conf.d/referral-spam.conf
 
 ### More nginx blacklist from mariusv (add it to block.conf)
-### \curl -sSL https://raw.githubusercontent.com/gagomap/nginx-badbot-blocker/master/blacklist.conf > /etc/nginx/conf.d/nginx-badbot-blocker.conf
+echo "Install nginx blacklist from mariusv (bad_mariusv_referer and bad_mariusv_bot) "
+\curl -sSL https://github.com/gagomap/nginx-badbot-blocker/blob/master/blacklist.conf > /etc/nginx/conf.d/nginx-badbot-blocker.conf
 
 ### More bots blocker from gagomap (limit_bots and ggbad_bots)
-echo "Install more bots blocker from gagomap (limit_bots and ggbad_bots) "
-\curl -sSL https://raw.githubusercontent.com/gagomap/nginx-block-bots/master/block.conf > /etc/nginx/conf.d/block.conf
-\curl -sSL https://raw.githubusercontent.com/gagomap/nginx-block-bots/master/gg-blacklist1.conf > /etc/nginx/conf.d/gg-blacklist1.conf
+# echo "Install more bots blocker from gagomap (limit_bots and ggbad_bots) "
+# \curl -sSL https://raw.githubusercontent.com/gagomap/nginx-block-bots/master/block.conf > /etc/nginx/conf.d/block.conf
+# \curl -sSL https://raw.githubusercontent.com/gagomap/nginx-block-bots/master/gg-blacklist1.conf > /etc/nginx/conf.d/gg-blacklist1.conf
 
 ### 4G Bots and Referer Blocker from gagomap
-echo "Install 4G Bots and Referer (4gbad_bots and 4gbad_referers) "
-\curl -sSL https://raw.githubusercontent.com/gagomap/4g-blacklists/master/4g-referer.conf > /etc/nginx/conf.d/4g-referer.conf
-\curl -sSL https://raw.githubusercontent.com/gagomap/4g-blacklists/master/4g-badbots.conf > /etc/nginx/conf.d/4g-badbots.conf
+# echo "Install 4G Bots and Referer (4gbad_bots and 4gbad_referers) "
+# \curl -sSL https://raw.githubusercontent.com/gagomap/4g-blacklists/master/4g-referer.conf > /etc/nginx/conf.d/4g-referer.conf
+# \curl -sSL https://raw.githubusercontent.com/gagomap/4g-blacklists/master/4g-badbots.conf > /etc/nginx/conf.d/4g-badbots.conf
 
 ### Create Piwik blacklist (piwik_referer)
 echo "Install Piwik blacklist (piwik_referer) "
